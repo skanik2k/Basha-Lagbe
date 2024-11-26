ee0e54cd97f1c3cfeace3be1842cd03d5536405e
skanik2k <116240698+skanik2k@users.noreply.github.com>
2024-11-26 21:07:51 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<?php 
+ini_set('session.cache_limiter','public');
+session_cache_limiter(false);
+session_start();
+include("config.php");
+								
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- FOR MORE PROJECTS visit: codeastro.com -->
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Meta Tags -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<!--	Fonts
+	========================================================-->
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+<!--	Css Link
+	========================================================-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css" id="color-change">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<!--	Title
+	=========================================================-->
+<title>Real Estate PHP</title>
+</head>
+<body>
+
+<!--	Page Loader  -->
+<!--<div class="page-loader position-fixed z-index-9999 w-100 bg-white vh-100">
+	<div class="d-flex justify-content-center y-middle position-relative">
+	  <div class="spinner-border" role="status">
+		<span class="sr-only">Loading...</span>
+	  </div>
+	</div>
+</div>  -->
+<!--	Page Loader  -->
+
+<div id="page-wrapper">
+    <div class="row"> 
+        <!--	Header start  -->
+		<?php include("include/header.php");?>
+        <!--	Header end  -->
+		
+        <!--	Banner Start   -->
+        <div class="overlay-black w-100 slider-banner1 position-relative" style="background-image: url('images/banner/rshmpg.jpg'); background-size: 100% 100%; background-position: center center; background-repeat: no-repeat;"></div>
+        <!--	Banner End  -->
+        
+        <!--	Text Block One
+		======================================================-->
+        
+		
+		
+		
+        <!--	Footer   start-->
+		
+		<!--	Footer   start-->
+        
+        
+        <!-- Scroll to top --> 
+        <a href="#" class="bg-success text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+        <!-- End Scroll To top --> 
+    </div>
+</div>
+<!-- Wrapper End --> 
+
+<!--	Js Link
+============================================================--> 
+<script src="js/jquery.min.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+<script src="js/YouTubePopUp.jquery.js"></script> 
+<script src="js/validate.js"></script> 
+<script src="js/jquery.cookie.js"></script> 
+<script src="js/custom.js"></script>
+</body>
+
+</html>
